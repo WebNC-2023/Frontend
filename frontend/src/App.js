d726bf701479aabd2ec43163629187ddc6067fe9
Huuthien1404 <thie1232135@gmail.com>
2023-11-16 04:01:52 +0700

    first commit hompage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,14 @@ import {
   Navigate,
 } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import HomePage from "./pages/HomePage/HomePage";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/*" element={<Navigate to="/landing-page" />} />
         <Route path="/landing-page" element={<LandingPage />} />
+        <Route path="/home-page" element={<HomePage />} />
       </Routes>
     </Router>
   );
