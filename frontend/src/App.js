f7be860a22ff8fc0af35a3c098306931201ee241
ltk2801 <luutuankhanha3dt@gmail.com>
2023-11-18 15:36:41 +0700

    error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,44 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import HomePage from "./pages/HomePage/HomePage";
 import Login from "./pages/LoginPage/Login";
 import Register from "./pages/RegisterPage/Register";
+import Error from "./pages/ErrorPage/Error";
 import ToastContainer from "./components/Notifications/ToastContainer";
 import React from "react";
 import { DataContext } from "./contexts/DataContext";
 function App() {
-  let firstUserName = JSON.parse(localStorage.getItem("userInfo"))?.firstName || "";
-  let lastUserName =  JSON.parse(localStorage.getItem("userInfo"))?.lastName || "";
+  let firstUserName =
+    JSON.parse(localStorage.getItem("userInfo"))?.firstName || "";
+  let lastUserName =
+    JSON.parse(localStorage.getItem("userInfo"))?.lastName || "";
   const [showSmallMenu, setShowSmallMenu] = useState(false);
   const [showContent, setShowContent] = useState("Home");
   const [showScreen, setShowScreen] = useState("courses");
   const [fullName, setFullName] = useState(`${firstUserName} ${lastUserName}`);
   return (
     <DataContext.Provider
-      value={{ showSmallMenu, setShowSmallMenu, showContent, setShowContent, showScreen, setShowScreen, fullName, setFullName }}
+      value={{
+        showSmallMenu,
+        setShowSmallMenu,
+        showContent,
+        setShowContent,
+        showScreen,
+        setShowScreen,
+        fullName,
+        setFullName,
+      }}
     >
       <Router>
         <ToastContainer />
         <Routes>
-          <Route path="/*" element={<Navigate to="/landing-page" />} />
-
-          <Route path="/landing-page" element={<LandingPage />} />
-
+          <Route path="/" element={<LandingPage />} />
           <Route path="/home-page" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
     </DataContext.Provider>
