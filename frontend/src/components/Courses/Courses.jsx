a3a6f27d7c64aed4c8254bce85ae072013d971e5
Huuthien1404 <thie1232135@gmail.com>
2023-11-19 01:26:49 +0700

    added data

--- frontend/src/components/Courses/Courses.jsx
+++ frontend/src/components/Courses/Courses.jsx
@@ -83,6 +83,80 @@ let courses_list = [
         course_name: "Master of Information Technology"
     },
 ];
+let best_courses = [
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/20/8f15cc441443de8a35271d25610e22/Google-cybersecurity.jpeg?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/4a/cb36835ae3421187080898a7ecc11d/Google-G_360x360.png",
+        university_name: "Google",
+        course_name: "Google Cybersecurity Professional Certificate"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/23/22bc54f77f45a2b057f4ff518d272f/iStock-1169539468.jpg?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/bb/f5ced2bdd4437aa79f00eb1bf7fbf0/IBM-Logo-Blk---Square.png",
+        university_name: "IBM",
+        course_name: "IBM Data Science Professional Certificate"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://s3.amazonaws.com/coursera-course-photos/25/100f488610437c8409a9af3d5a8066/DeepLearning_Generative_AI_for_Everyone_Banner_1000x1000_F.png?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/b4/5cb90bb92f420b99bf323a0356f451/Icon.png",
+        university_name: "DeepLearning.AI",
+        course_name: "Generative AI for Everyone"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/1e/9830d71e1d48ba8b883e3912b9a144/IBM-Digital-Strategy_Specialization_600x600b.jpg?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/d4/6c1c1ee0ca48c5993298bb2e0c7f46/Darden_Foundation_Monogram_360x360.png",
+        university_name: "University of Virginia Darden School Foundation",
+        course_name: "IBM & Darden Digital Strategy Specialization"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/ed/d25c0d25114924a34754928dbf8273/Front-end-dev-ProCert.png?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&q=50&fit=crop",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/73/e03b13a8e44df9b19eb279e5506396/360-x-360.png",
+        university_name: "Meta",
+        course_name: "Meta Front-End Developer Professional Certificate"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/0a/78a20b48fe419ea3f857ea43e3e38b/IT-Project-Management-PC.jpg?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/57/2f1c35bd4a4a6d94bde7d622267b9c/Partner-landing-page-Logo-2.png",
+        university_name: "skillUp EdTech",
+        course_name: "IBM IT Project Manager Professional Certificate"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/8c/e0f2fd124b441fa8d9aa68fdab2167/Marketing-Image.png?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/cc/61dbdf2c1c475d82d3b8bf8eee1bda/MSFT-stacked-logo_FINAL.png",
+        university_name: "Microsoft",
+        course_name: "Microsoft Power BI Data Analyst Professional Certificate"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/16/5e0361da674644abdad374133539d2/sparc_600.png?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/89/63fef0315140268d5c0f66eee8e85e/VU_360x360.png",
+        university_name: "Vanderbilt University",
+        course_name: "Prompt Engineering Specialization"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d396qusza40orc.cloudfront.net/phoenixassets/wharton-business-foundations/wharton-business-foundations-logo.jpg?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/a2/66aaaad14d426fa9798ed714b3d0e5/UniversityofPennsylvania_Vertical_RGB_coursera-cert.png",
+        university_name: "University of Pennsylvania",
+        course_name: "Business Foundations Specialization"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d2j5ihb19pt1hq.cloudfront.net/sdp_page/s12n_logos/python.jpg?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&q=50&fit=crop",
+        university_img: "https://coursera-university-assets.s3.amazonaws.com/70/de505d47be7d3a063b51b6f856a6e2/New-Block-M-Stacked-Blue-295C_600x600.png",
+        university_name: "University of Michigan",
+        course_name: "Python for Everybody Specialization"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/c0/7c9805bdef4f32a8b7aef5b5ba465e/Specialization-Image_600x600.png?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/e8/7cc3d09d3f11e698dfff46d35f2da1/Stanford_Coursera_Logo.png",
+        university_name: "Stanford University",
+        course_name: "AI in Healthcare"
+    },
+    {
+        course_img: "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://d15cw65ipctsrr.cloudfront.net/a4/0ee0209fb444a38ade1afccde6355e/option2.jpg?auto=format%2Ccompress%2C%20enhance&dpr=2&w=265&h=216&fit=crop&q=50",
+        university_img: "http://coursera-university-assets.s3.amazonaws.com/0e/4bb2b81f8e453eab147bbb94bb53da/Intuit_Square_360x360_WhiteonBlue.png",
+        university_name: "Intuit",
+        course_name: "Intuit Academy Bookkeeping Professional Certificate"
+    },
+]
 const Courses = () => {
     const itemsPerPage = 8;
     const [pageCount, setPageCount] = useState(0);
@@ -136,7 +210,7 @@ const Courses = () => {
                         }
                     }
                 >
-                    {courses_list.map((course, id) => {
+                    {best_courses.map((course, id) => {
                         return (
                             <SwiperSlide key={id}>
                                 <div className="course-item-swiper">
