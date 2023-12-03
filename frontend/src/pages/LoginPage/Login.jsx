9efb25d6f21507999668bf29a226dee5a10a3077
ltk2801 <luutuankhanha3dt@gmail.com>
2023-12-03 16:49:02 +0700

    update UI login/register page

--- frontend/src/pages/LoginPage/Login.jsx
+++ frontend/src/pages/LoginPage/Login.jsx
@@ -64,176 +64,201 @@ export default function SignIn() {
   }, [userInfo, isSuccess, isError, navigate, dispatch]);
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
+      <div
+        style={{
+          backgroundColor: "#f0f3f5",
+          width: "100%",
+          height: "100vh",
+          margin: "0",
+          padding: "0",
+        }}
+      >
+        <Container
+          component="main"
+          maxWidth="xs"
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "80vh",
+            backgroundColor: "white",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+            borderRadius: "8px",
+            minHeight: "70vh",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            padding: "24px",
           }}
         >
-          <Avatar
-            sx={{ m: 1, bgcolor: "#5175e0", width: "50px", height: "50px" }}
-          >
-            <LockOutlinedIcon />
-          </Avatar>
-
-          <Typography component="h1" variant="h4">
-            Sign in
-          </Typography>
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={formik.handleSubmit}
-            sx={{ mt: 5 }}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type={show ? "text" : "password"}
-              id="password"
-              autoComplete="current-password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-              helperText={formik.touched.password && formik.errors.password}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => {
-                        setShow(!show);
-                      }}
-                    >
-                      {show ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              sx={{ mb: 2 }}
-            />
-            <Link
-              href="/forgot-password"
-              variant="body2"
-              style={{
-                textDecoration: "none",
-                fontWeight: "bold",
-                fontStyle: "italic",
-              }}
+            <Avatar
+              sx={{ m: 1, bgcolor: "#5175e0", width: "50px", height: "50px" }}
             >
-              {"Forgot Password?"}
-            </Link>
+              <LockOutlinedIcon />
+            </Avatar>
 
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              disabled={isLoading}
-              sx={{
-                mt: 3,
-                mb: 2,
-                bgcolor: "#5175e0",
-                "&:hover": {
+            <Typography component="h1" variant="h4">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={formik.handleSubmit}
+              sx={{ mt: 5 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type={show ? "text" : "password"}
+                id="password"
+                autoComplete="current-password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => {
+                          setShow(!show);
+                        }}
+                      >
+                        {show ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                sx={{ mb: 2 }}
+              />
+              <Link
+                href="/forgot-password"
+                variant="body2"
+                style={{
+                  textDecoration: "none",
+                  fontWeight: "bold",
+                  fontStyle: "italic",
+                }}
+              >
+                {"Forgot Password?"}
+              </Link>
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                disabled={isLoading}
+                sx={{
+                  mt: 3,
+                  mb: 2,
                   bgcolor: "#5175e0",
-                },
-              }}
-            >
-              {isLoading ? "Loading..." : "Sign In"}
-            </Button>
-            <Grid container>
-              {/* <Grid item xs></Grid> */}
-              <Grid item>
-                <Link
-                  href="/register"
-                  variant="body2"
-                  style={{
-                    textDecoration: "none",
-                  }}
-                >
-                  {"Don't have an account? Sign Up"}
-                </Link>
+                  "&:hover": {
+                    bgcolor: "#5175e0",
+                  },
+                }}
+              >
+                {isLoading ? "Loading..." : "Sign In"}
+              </Button>
+              <Grid container>
+                {/* <Grid item xs></Grid> */}
+                <Grid item>
+                  <Link
+                    href="/register"
+                    variant="body2"
+                    style={{
+                      textDecoration: "none",
+                    }}
+                  >
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
-            <Divider
-              sx={{
-                mt: 2,
-                mb: 2,
-                color: "#868e96",
-                fontSize: "14px",
-              }}
-            >
-              OR
-            </Divider>
-            <Stack
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              spacing={3}
-              style={{
-                gap: "0.5rem",
-              }}
-            >
-              <FacebookLoginButton
-                style={{
-                  background: "#5175e0",
-                  gap: "1rem",
-                  border: "1px solid #5175e0",
+              <Divider
+                sx={{
+                  mt: 2,
+                  mb: 2,
+                  color: "#868e96",
+                  fontSize: "14px",
                 }}
-                activeStyle={{
-                  background: "#3c5fc9",
-                  boxShadow: "0 0 5px rgba(0, 0, 0, 0.05)",
-                }}
-                iconSize="20px"
-                onClick={handleFacebookLogin}
               >
-                <span
-                  style={{
-                    marginLeft: "0.5rem",
-                  }}
-                >
-                  Continue with Facebook
-                </span>
-              </FacebookLoginButton>
-              <GoogleLoginButton
+                OR
+              </Divider>
+              <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={3}
                 style={{
-                  border: "1px solid #f5f5f5",
+                  gap: "0.5rem",
                 }}
-                iconSize="20px"
-                onClick={handleGoogleLogin}
               >
-                <span
+                <FacebookLoginButton
                   style={{
-                    marginLeft: "0.5rem",
+                    background: "#5175e0",
+                    gap: "1rem",
+                    border: "1px solid #5175e0",
                   }}
+                  activeStyle={{
+                    background: "#3c5fc9",
+                    boxShadow: "0 0 5px rgba(0, 0, 0, 0.05)",
+                  }}
+                  iconSize="20px"
+                  onClick={handleFacebookLogin}
                 >
-                  Continue with Google{" "}
-                </span>
-              </GoogleLoginButton>
-            </Stack>
+                  <span
+                    style={{
+                      marginLeft: "0.5rem",
+                    }}
+                  >
+                    Continue with Facebook
+                  </span>
+                </FacebookLoginButton>
+                <GoogleLoginButton
+                  style={{
+                    border: "1px solid #f5f5f5",
+                  }}
+                  iconSize="20px"
+                  onClick={handleGoogleLogin}
+                >
+                  <span
+                    style={{
+                      marginLeft: "0.5rem",
+                    }}
+                  >
+                    Continue with Google{" "}
+                  </span>
+                </GoogleLoginButton>
+              </Stack>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+        </Container>
+      </div>
     </ThemeProvider>
   );
 }
--- frontend/src/pages/RegisterPage/Register.jsx
+++ frontend/src/pages/RegisterPage/Register.jsx
@@ -75,200 +75,228 @@ export default function Register() {
   });
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
+      <div
+        style={{
+          backgroundColor: "#f0f3f5",
+          width: "100%",
+          height: "100vh",
+          margin: "0",
+          padding: "0",
+        }}
+      >
+        <Container
+          component="main"
+          maxWidth="xs"
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "80vh",
+            backgroundColor: "white",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+            borderRadius: "8px",
+            minHeight: "70vh",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            padding: "24px",
           }}
         >
-          <Avatar
-            sx={{ m: 1, bgcolor: "#5175e0", width: "50px", height: "50px" }}
-          >
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h4">
-            Sign up
-          </Typography>
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={formik.handleSubmit}
-            sx={{ mt: 5 }}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                  value={formik.values.firstName}
-                  onChange={formik.handleChange}
-                  error={
-                    formik.touched.firstName && Boolean(formik.errors.firstName)
-                  }
-                  helperText={
-                    formik.touched.firstName && formik.errors.firstName
-                  }
-                />
+            <Avatar
+              sx={{ m: 1, bgcolor: "#5175e0", width: "50px", height: "50px" }}
+            >
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h4">
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={formik.handleSubmit}
+              sx={{ mt: 5 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    value={formik.values.firstName}
+                    onChange={formik.handleChange}
+                    error={
+                      formik.touched.firstName &&
+                      Boolean(formik.errors.firstName)
+                    }
+                    helperText={
+                      formik.touched.firstName && formik.errors.firstName
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                    value={formik.values.lastName}
+                    onChange={formik.handleChange}
+                    error={
+                      formik.touched.lastName && Boolean(formik.errors.lastName)
+                    }
+                    helperText={
+                      formik.touched.lastName && formik.errors.lastName
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type={show ? "text" : "password"}
+                    id="password"
+                    autoComplete="new-password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={
+                      formik.touched.password && Boolean(formik.errors.password)
+                    }
+                    helperText={
+                      formik.touched.password && formik.errors.password
+                    }
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={() => {
+                              setShow(!show);
+                            }}
+                          >
+                            {show ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                  value={formik.values.lastName}
-                  onChange={formik.handleChange}
-                  error={
-                    formik.touched.lastName && Boolean(formik.errors.lastName)
-                  }
-                  helperText={formik.touched.lastName && formik.errors.lastName}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  helperText={formik.touched.email && formik.errors.email}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type={show ? "text" : "password"}
-                  id="password"
-                  autoComplete="new-password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  error={
-                    formik.touched.password && Boolean(formik.errors.password)
-                  }
-                  helperText={formik.touched.password && formik.errors.password}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={() => {
-                            setShow(!show);
-                          }}
-                        >
-                          {show ? <Visibility /> : <VisibilityOff />}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              disabled={isLoading}
-              sx={{
-                mt: 3,
-                mb: 2,
-                bgcolor: "#5175e0",
-                "&:hover": {
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                disabled={isLoading}
+                sx={{
+                  mt: 3,
+                  mb: 2,
                   bgcolor: "#5175e0",
-                },
-              }}
-            >
-              {isLoading ? "Loading..." : "Sign up"}
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link
-                  href="/login"
-                  variant="body2"
-                  sx={{ color: "#5175e0" }}
-                  style={{
-                    textDecoration: "none",
-                  }}
-                >
-                  Already have an account? Sign in
-                </Link>
+                  "&:hover": {
+                    bgcolor: "#5175e0",
+                  },
+                }}
+              >
+                {isLoading ? "Loading..." : "Sign up"}
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link
+                    href="/login"
+                    variant="body2"
+                    sx={{ color: "#5175e0" }}
+                    style={{
+                      textDecoration: "none",
+                    }}
+                  >
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
-            <Divider
-              sx={{
-                mt: 2,
-                mb: 2,
-                color: "#868e96",
-                fontSize: "14px",
-              }}
-            >
-              OR
-            </Divider>
-            <Stack
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              spacing={3}
-              style={{
-                gap: "0.5rem",
-              }}
-            >
-              <FacebookLoginButton
-                style={{
-                  background: "#5175e0",
-                  gap: "1rem",
-                  border: "1px solid #5175e0",
+              <Divider
+                sx={{
+                  mt: 2,
+                  mb: 2,
+                  color: "#868e96",
+                  fontSize: "14px",
                 }}
-                activeStyle={{
-                  background: "#3c5fc9",
-                  boxShadow: "0 0 5px rgba(0, 0, 0, 0.05)",
-                }}
-                onClick={handleFacebookLogin}
               >
-                <span
-                  style={{
-                    marginLeft: "0.5rem",
-                  }}
-                >
-                  Continue with Facebook
-                </span>
-              </FacebookLoginButton>
-              <GoogleLoginButton
+                OR
+              </Divider>
+              <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={3}
                 style={{
-                  border: "1px solid #f5f5f5",
+                  gap: "0.5rem",
                 }}
-                onClick={handleGoogleLogin}
               >
-                <span
+                <FacebookLoginButton
                   style={{
-                    marginLeft: "0.5rem",
+                    background: "#5175e0",
+                    gap: "1rem",
+                    border: "1px solid #5175e0",
                   }}
+                  activeStyle={{
+                    background: "#3c5fc9",
+                    boxShadow: "0 0 5px rgba(0, 0, 0, 0.05)",
+                  }}
+                  onClick={handleFacebookLogin}
                 >
-                  Continue with Google{" "}
-                </span>
-              </GoogleLoginButton>
-            </Stack>
+                  <span
+                    style={{
+                      marginLeft: "0.5rem",
+                    }}
+                  >
+                    Continue with Facebook
+                  </span>
+                </FacebookLoginButton>
+                <GoogleLoginButton
+                  style={{
+                    border: "1px solid #f5f5f5",
+                  }}
+                  onClick={handleGoogleLogin}
+                >
+                  <span
+                    style={{
+                      marginLeft: "0.5rem",
+                    }}
+                  >
+                    Continue with Google{" "}
+                  </span>
+                </GoogleLoginButton>
+              </Stack>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+        </Container>
+      </div>
     </ThemeProvider>
   );
 }
