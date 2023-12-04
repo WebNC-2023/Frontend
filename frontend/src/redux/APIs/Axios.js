be7d4db615e434bae4647de54390caeef74f58c1
ltk2801 <luutuankhanha3dt@gmail.com>
2023-12-04 13:24:05 +0700

    fix Axios

--- frontend/src/redux/APIs/Axios.js
+++ frontend/src/redux/APIs/Axios.js
@@ -4,39 +4,41 @@ const Axios = axios.create({
   withCredentials: true,
 });
 
+// Tạm thời ẩn cái interceptor này, tại vì nếu window.location = '/login' thì mọi lỗi về chuyển về login hết
+
 // Response interceptor để xử lý refresh token khi token hết hạn
-Axios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    console.log("Access token expired");
-    window.location = "/login";
-    // Kiểm tra nếu lỗi là do token hết hạn và chưa thử refresh token
-    //console.log(error.response.status, originalRequest._retry, refreshToken);
-    // if (
-    //   error.response &&
-    //   error.response.status === 401 &&
-    //   error.response.data === "Unauthorized"
-    // ) {
-    // try {
-    //   console.log("call refresh token");
-    //   await Axios.get("/auth/refresh");
-    //   return Axios.request(error.config);
-    // } catch (err) {
-    //   localStorage.removeItem("userInfo");
-    //   setTimeout(
-    //     () => (window.location.href = "http://localhost:3000/login"),
-    //     3000
-    //   );
+// Axios.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     console.log("Access token expired");
+//     window.location = "/login";
+//     // Kiểm tra nếu lỗi là do token hết hạn và chưa thử refresh token
+//     //console.log(error.response.status, originalRequest._retry, refreshToken);
+//     // if (
+//     //   error.response &&
+//     //   error.response.status === 401 &&
+//     //   error.response.data === "Unauthorized"
+//     // ) {
+//     // try {
+//     //   console.log("call refresh token");
+//     //   await Axios.get("/auth/refresh");
+//     //   return Axios.request(error.config);
+//     // } catch (err) {
+//     //   localStorage.removeItem("userInfo");
+//     //   setTimeout(
+//     //     () => (window.location.href = "http://localhost:3000/login"),
+//     //     3000
+//     //   );
 
-    //   console.log(err);
+//     //   console.log(err);
 
-    //   return Promise.reject(err);
-    // }
-    // }
-    // return Promise.reject(error);
-  }
-);
+//     //   return Promise.reject(err);
+//     // }
+//     // }
+//     // return Promise.reject(error);
+//   }
+// );
 
 export default Axios;
