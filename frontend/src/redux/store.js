cd92830e80b7f7eddf0a7f077b976db94739dedc
ltk2801 <luutuankhanha3dt@gmail.com>
2023-12-10 01:39:27 +0700

    Api edit class

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -14,6 +14,7 @@ const rootReducer = combineReducers({
   classroomComment: classroomCommentReducer,
   createClass: Class.classCreateReducer,
   classes: Class.classGetReducer,
+  editClass: Class.classEditReducer,
 });
 
 // get userInfo from localStorage
